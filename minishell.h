418ad1fdf7f892743a3573e70280f2349e723364
田中　掌 <tanaka_tsukasa@tsukasa-no-MacBook-Pro.local>
2025-06-10 14:51:58 +0900

    ast構造体

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sguruge <sguruge@student.42tokyo.jp>       #+#  +:+       +#+        */
+/*   By: ttanaka <ttanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-06-08 09:49:58 by sguruge           #+#    #+#             */
-/*   Updated: 2025-06-08 09:49:58 by sguruge          ###   ########.fr       */
+/*   Created: 2025/06/08 09:49:58 by sguruge           #+#    #+#             */
+/*   Updated: 2025/06/10 14:51:46 by ttanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,43 @@ typedef struct s_token
 	struct s_token	*prev;
 }					t_token;
 
+typedef enum e_redirect_kind{
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_APPEND,
+	REDIR_HEREDOC
+} t_redirect_kind;
+
+typedef enum e_node_kind{
+	NODE_PIPE, /* | */
+	NODE_AND, /* '&&' */
+	NODE_OR,  /* '||' */
+	NODE_SIMPLE_COMMAND
+} t_node_kind;
+
+typedef struct s_redirect{
+	t_redirect_kind kind;
+	int io_number;
+	char *filename;
+	struct s_redirect *next;
+} t_redirect;
+
+typedef struct s_command{
+	char **args;
+	t_redirect *redirects;
+} t_command;
+
+union u_data{
+	t_command command;
+};
+
+typedef struct s_tree_node{
+	t_node_kind kind;
+	struct s_tree_node *left;
+	struct s_tree_node *right;
+	u_data data;
+	bool is_background;
+} t_tree_node;
+
 #endif
+
