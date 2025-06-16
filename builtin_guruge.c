4afd8c44780d97d926e2ab2400a24b71b59c1ea2
Shion Guruge <sguruge@c2r5s8.42tokyo.jp>
2025-06-17 00:09:40 +0900

    builtin

--- builtin_guruge.c
+++ builtin_guruge.c
@@ -3,4 +3,13 @@
 
 unsigned char	builtin_cd(t_tree_node *node, t_env *env)
 {
+	char	*path;
+
+	if (node->data.command.args[2])
+		return (1); // INVALid SYNTAX
+	else if (!node->data.command.args[1])
+		return (chdir($HOME)); // MOVE HOME
+	node->data.command.args[1] = path;
+	if (ft_strncmp(path, "-", 2) == 0)
+		return (chdir($OLDPWD));
 }
