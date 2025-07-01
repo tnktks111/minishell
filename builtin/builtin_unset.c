10a6bd41b5f7caa348ebb62efc2dce0c151516e5
Tsukasa Tanaka <ttanaka@c1r2s1.42tokyo.jp>
2025-07-01 14:51:20 +0900

    builtin_env修正

--- builtin/builtin_unset.c
+++ builtin/builtin_unset.c
@@ -6,46 +6,37 @@
 /*   By: ttanaka <ttanaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 10:41:59 by ttanaka           #+#    #+#             */
-/*   Updated: 2025/06/28 23:02:14 by ttanaka          ###   ########.fr       */
+/*   Updated: 2025/07/01 14:49:33 by ttanaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int		is_valid_unset_key(char *s);
+static bool	is_valid_option(char *s);
 unsigned char	builtin_unset(t_tree_node *node, t_env *env);
 
-static int	is_valid_unset_key(char *s)
+static bool	is_valid_option(char *s)
 {
-	if (ft_strcmp(s, "?") == 0)
-		return (1);
-	while (*s)
-	{
-		if (!(ft_isalnum(*s) || *s == '_'))
-			return (0);
-		s++;
-	}
-	return (1);
+	if (s[0] == '-' && s[1] && s[1] != '-')
+		return (false);
+	if (s[0] == '-' && s[1] == '-' && s[2])
+		return (false);
+	return (true);
 }
 
 unsigned char	builtin_unset(t_tree_node *node, t_env *env)
 {
 	size_t			i;
-	unsigned char	exit_status;
 
-	i = 0;
-	exit_status = EXIT_SUCCESS;
-	while (node->data.command.args[++i])
+	if (node->data.command.args[1]
+		&& !is_valid_option(node->data.command.args[1]))
 	{
-		if (is_valid_unset_key(node->data.command.args[i]))
-		{
-			ft_remove_key(node->data.command.args[i], env);
-		}
-		else
-		{
-			error_not_a_valid_identifier("unset", node->data.command.args[i]);
-			exit_status = EXIT_FAILURE;
-		}
+		error_invalid_option(node->data.command.args[0],
+			node->data.command.args[1]);
+		return (2);
 	}
-	return (exit_status);
+	i = 0;
+	while (node->data.command.args[++i])
+		ft_remove_key(node->data.command.args[i], env);
+	return (0);
 }
